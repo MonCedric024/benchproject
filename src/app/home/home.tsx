2ec2c5def6187f259d490f0fabc964d7be083571
Mon Cedric Avila <mon.cedric.avila@accenture.com>
2024-11-11 17:12:01 +0800

    frontend

--- src/app/home/home.tsx
+++ src/app/home/home.tsx
@@ -1,5 +1,13 @@
 export default function Page() {
     return (
-    <h1>Main</h1>
+        <div className="flex justify-center bg-black min-h-screen">
+            <img 
+                src="https://th.bing.com/th/id/OIP.z8kax9MsN3MOuMlSPHEzLgHaEK?rs=1&pid=ImgDetMain" 
+                className="w-full h-80 object-cover"
+                alt="Full Width Image"
+            />
+            <img src="https://scontent.fmnl7-1.fna.fbcdn.net/v/t39.30808-6/431170570_1784594818634280_7274865459438706259_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=LQGkIopTv20Q7kNvgHJwoCe&_nc_zt=23&_nc_ht=scontent.fmnl7-1.fna&_nc_gid=AtBEqRZNnZIYFJXdW3ffjrH&oh=00_AYBNTokvKAJnCRpP3B5eUvM6SNiNvBWEJ4cv0-r_rwxGew&oe=67379E2D" 
+                className="absolute top-60 border-solid border-4 border-black rounded-full w-32 h-32"></img>
+        </div>
     )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <title>My Application</title>
       </head>
       <body>
-        <Header />
-          <main className="col-span-1 md:col-span-9 order-1 md:order-2 p-2 md:p-5">
+        {/* <Header /> */}
+          <main className="col-span-1 sm:pr-[1rem] sm:pl-[1rem] md:pr-[10rem] md:pl-[10rem] lg:pr-[25rem] lg:pl-[25rem] 2xl:pr-[40rem] 2xl:pl-[40rem] md:col-span-9 order-1 md:order-2">
             {children}
           </main>
         <Footer />
